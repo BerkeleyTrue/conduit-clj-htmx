596cc03e62f0a62137cad6e8b8463e5078028e87
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-12-14 22:38:06 -0700

    feat: move to multiple envs

--- src/conduit/core.clj
+++ src/conduit/core.clj
@@ -1,12 +1,32 @@
 (ns conduit.core
   (:require
-    [aero.core :as aero]
-    [integrant.core :as ig]))
+   [clojure.tools.logging :as log]
+   [aero.core :as aero]
+   [integrant.core :as ig]))
 
 (defmethod aero/reader 'ig/ref [_ _ value]
   (ig/ref value))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& _]
-  (println "Hello, World!"))
+;; log uncaught exceptions in threads
+(Thread/setDefaultUncaughtExceptionHandler
+ (reify Thread$UncaughtExceptionHandler
+   (uncaughtException [_ thread ex]
+     (log/error {:what :uncaught-exception
+                 :exception ex
+                 :where (str "Uncaught exception on" (.getName thread))}))))
+
+(defonce system (atom nil))
+
+(defn stop-app []
+  (some-> (deref system) (ig/halt!))
+  (shutdown-agents))
+
+(defn start-app [& _]
+  (->> (:server (aero/read-config "config.edn"))
+       (ig/prep)
+       (ig/init)
+       (reset! system))
+  (.addShutdownHook (Runtime/getRuntime) (Thread. stop-app)))
+
+(defn -main [& _]
+  (start-app))
