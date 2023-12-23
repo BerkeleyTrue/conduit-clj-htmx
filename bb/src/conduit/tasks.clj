7711eb215c3949d22cb08d4d97fa4e68c3f67ded
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-12-22 20:19:07 -0700

    fix(bb): remove aero requirement

--- bb/src/conduit/tasks.clj
+++ bb/src/conduit/tasks.clj
@@ -1,17 +1,19 @@
 (ns conduit.tasks
   (:require
-   [aero.core :as aero]
    [babashka.tasks :as tasks :refer [clojure]]
    [clojure.java.io :as io]))
 
 (def config
-  (delay (aero/read-config "tasks.edn")))
+  {:clj-args ["-J-XX:-OmitStackTraceInFastThrow"
+              "-J-XX:+CrashOnOutOfMemoryError"
+              "-J-Duser.timezone=UTC"
+              "-M:dev"]})
 
 (defn dev
   "Starts the app in dev mode"
   [& args]
   (io/make-parents "target/resources/_")
-  (apply clojure (concat args (:clj-args @config))))
+  (apply clojure (concat args (:clj-args config))))
 
 (comment
   (dev))
