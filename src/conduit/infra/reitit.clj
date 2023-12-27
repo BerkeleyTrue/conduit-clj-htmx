bd6811aabdc2e0d715a0cc331cf26d235f9018c8
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-12-27 00:22:37 -0700

    wip: switch to reitit-ring-defaults

--- src/conduit/infra/reitit.clj
+++ src/conduit/infra/reitit.clj
@@ -1,16 +1,40 @@
 (ns conduit.infra.reitit
   (:require
+   [taoensso.timbre :as timbre]
    [integrant.core :as ig]
-   [taoensso.timbre :refer [info]]
+   [muuntaja.format.form :as muu.form]
    [ring.util.response :as response]
+   [ring.middleware.defaults :refer [site-defaults]]
+   [reitit.coercion.malli :as coercion.malli]
    [reitit.ring :as ring]
-
-   [conduit.infra.ring :refer [->middleware]]))
+   [reitit.ring.coercion :as coercion]
+   [reitit.ring.middleware.defaults :refer [ring-defaults-middleware]]
+   [reitit.ring.middleware.parameters :as parameters]
+   [reitit.ring.middleware.muuntaja :as muu.reitit]
+   [reitit.ring.middleware.exception :as exception]
+   [conduit.env :as env]))
 
 (defmethod ig/init-key :infra.router/core
   [_ {:keys [routes] :as opts}]
-  (info "init router " opts)
-  (ring/router ["" opts routes]))
+  (timbre/info "init router " opts)
+  (ring/router
+   ["" opts routes]
+   {:data {:coercion
+           (coercion.malli/create
+            {:transformers
+             {:body {:default reitit.coercion.malli/default-transformer-provider
+                     :formats {"application/x-www-form-urlencoded" muu.form/format}}}})
+           :middleware
+           [parameters/parameters-middleware
+            muu.reitit/format-negotiate-middleware
+            muu.reitit/format-request-middleware
+            exception/exception-middleware
+            muu.reitit/format-response-middleware
+            coercion/coerce-request-middleware
+            coercion/coerce-response-middleware
+            ring-defaults-middleware
+            (:middleware env/defaults)]
+           :defaults (-> site-defaults)}}))
 
 (defmethod ig/init-key :infra.ring/handler [_ {:keys [router]}]
   (let [default-handler (ring/routes ; default handler
@@ -35,7 +59,4 @@
                              {:status 406
                               :body "Not Acceptable"}
                              (response/content-type "text/html")))}))]
-    (->
-     router
-     (ring/ring-handler default-handler)
-     (->middleware))))
+    (ring/ring-handler router default-handler)))
--- src/conduit/infra/ring.clj
+++ src/conduit/infra/ring.clj
@@ -1,26 +1,16 @@
 (ns conduit.infra.ring
   (:require
    [integrant.core :as ig]
-   [muuntaja.middleware :as muu.middleware]
-   [muuntaja.format.form :as muu.form]
-   [muuntaja.core :as muu.core]
-   [ring.util.response :as response]
-   [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-   [conduit.env :as env]))
-
-(def m (muu.core/create
-         (->
-           muu.core/default-options
-           (assoc-in [:formats "application/x-www-form-urlencoded"] muu.form/format))))
+   [ring.util.response :as response]))
 
 (defmethod ig/init-key :infra.routes/health
   [_ _]
   ["/ping" {:name ::health
             :get (fn [_]
                    (->
-                     "pong"
-                     (response/response)
-                     (response/content-type "text/html")))}])
+                    "pong"
+                    (response/response)
+                    (response/content-type "text/html")))}])
 
 (derive :infra.routes/health :infra/routes)
 
@@ -30,11 +20,3 @@
 (defmethod ig/init-key :infra.router/routes
   [_ {:keys [infra-routes routes]}]
   (into [] (concat infra-routes routes)))
-
-(defn ->middleware [handler]
-  (->
-    handler
-    (muu.middleware/wrap-params)
-    (wrap-defaults (-> site-defaults (assoc-in [:params :urlencoded] false)))
-    (muu.middleware/wrap-format m)
-    ((:middleware env/defaults))))
