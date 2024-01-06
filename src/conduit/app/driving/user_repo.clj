52664562f4fd69acd0742def079365a4ed6d48a5
Berkeley True <berkeleytrue@users.noreply.github.com>
2024-01-06 12:45:36 -0700

    feat(user_repo): use new defact

--- src/conduit/app/driving/user_repo.clj
+++ src/conduit/app/driving/user_repo.clj
@@ -1,31 +1,34 @@
 (ns conduit.app.driving.user-repo
   (:require
    [datalevin.core :as d]
-   [integrant.core :as ig]))
+   [integrant.core :as ig]
+   [conduit.utils.dep-macro :refer [defact]]))
 
 (def user-schema
-   {:user/email
-    {:db/ident       :user/email
-     :db/valueType   :db.type/string
-     :db/cardinality :db.cardinality/one
-     :db/unique      :db.unique/identity}
+  {:user/email
+   {:db/ident       :user/email
+    :db/valueType   :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/unique      :db.unique/identity}
 
-    :user/username
-    {:db/ident       :user/username
-     :db/valueType   :db.type/string
-     :db/cardinality :db.cardinality/one
-     :db/unique      :db.unique/identity}})
+   :user/username
+   {:db/ident       :user/username
+    :db/valueType   :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/unique      :db.unique/identity}})
 
-(defn ->create-user [conn]
-  (println (type conn))
-  (fn create-user [{:keys [email password username created-at]}]
-    (d/transact! conn [{:db/id -1
-                        :user/username username
-                        :user/email email
-                        :user/password password
-                        :user/created-at created-at}])))
+(defact ->create-user
+  [conn]
+  {:pre [(d/conn? conn)]}
+  [{:keys [email password username created-at]}]
+  (d/transact! conn [{:db/id -1
+                      :user/username username
+                      :user/email email
+                      :user/password password
+                      :user/created-at created-at}]))
 
 (defn ->find-user-by-email [conn]
+  {:pre [(d/conn? conn)]}
   (fn find-user-by-email [{:keys [email]}]
     (let [query (d/q '[:find ?e ?email ?username ?password ?created-at
                        :in $ ?email
@@ -37,7 +40,6 @@
                      (d/db conn)
                      email)]
       (first query))))
-
 
 (defmethod ig/init-key :app.repos/user [_ {:keys [conn]}]
   (assert (d/conn? conn))
