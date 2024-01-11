0b7d48ca1ecce8644d7a95003f99671248082932
Berkeley True <berkeleytrue@users.noreply.github.com>
2024-01-11 09:21:59 -0700

    fix(utils): move list-errors into utils

--- src/conduit/infra/middleware/coercion.clj
+++ src/conduit/infra/middleware/coercion.clj
@@ -2,20 +2,7 @@
   (:require
    [reitit.coercion :as coercion]
    [ring.util.response :as response]
-   [conduit.infra.hiccup :refer [defhtml]]))
-
-(defhtml list-errors [errors-map]
-  [:ul#errors.error-messages
-   {:hx-swap-oob "true"}
-   (for [[k errs] errors-map]
-     (for [err errs]
-      [:li
-       (str (name k) ": " err)]))])
-
-(comment
-  (list-errors
-    {:email ["is required" "is not a valid email"]
-     :password ["is required" "must be at least 8 characters"]}))
+   [conduit.infra.utils :refer [list-errors]]))
 
 (defn handle-coercion-exception [e]
   (let [data (ex-data e)]
@@ -24,14 +11,15 @@
                       ::coercion/response-coercion 500
                       nil)]
       (->
-       {:status status
-        :body (->
-                (coercion/encode-error data)
-                (:humanized)
-                (list-errors)
-                (str))}
-       (response/header "HX-Push-URL" false)
-       (response/header "HX-Reswap" "none"))
+        {:status status
+          :body (->
+                  (coercion/encode-error data)
+                  (:humanized)
+                  (list-errors)
+                  (str))}
+        (response/header "HX-Push-URL" false)
+        (response/header "HX-Reswap" "none")
+        (response/content-type "text/html"))
       (throw e))))
 
 (def coerce-exceptions-htmx-middleware
--- src/conduit/infra/utils.clj
+++ src/conduit/infra/utils.clj
@@ -1,7 +1,7 @@
 (ns conduit.infra.utils
   (:require
    [ring.util.response :as response]
-   [conduit.infra.hiccup :refer []]))
+   [conduit.infra.hiccup :refer [defhtml]]))
 
 (defn response [hiccup-str]
   (->
@@ -9,3 +9,21 @@
     (str)
     (response/response)
     (response/content-type "text/html")))
+
+(defhtml list-errors [errsm]
+  [:div#errors.error-messages
+   {:hx-swap-oob :true} ; must be a string
+   (for [ky (keys errsm)
+         :let [errs (errsm ky)
+               errs (if (string? errs) [errs] errs)]]
+     (for [err errs]
+        [:li
+         (str (name ky) ": " err)]))])
+
+(comment
+  (list-errors {"email" "is invalid"
+                "password" "can't be blank"})
+  (list-errors {:email "is invalid"
+                :password "can't be blank"})
+  (list-errors {:email ["is invalid" "is too short (minimum is 1 character)"]
+                :password "can't be blank"}))
