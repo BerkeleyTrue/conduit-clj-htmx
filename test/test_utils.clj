18478bd1d529ee63de1c47a136824568a828a91b
Berkeley True <berkeleytrue@users.noreply.github.com>
2024-01-19 12:10:57 -0700

    feat(tests): add reporting to to-equal

--- test/test_utils.clj
+++ test/test_utils.clj
@@ -1,15 +1,38 @@
-(ns test-utils)
+(ns test-utils
+  (:require
+    [clojure.test :refer [do-report]]))
 
 
 (defmulti to-equal
   "Compare two values, with the same class, for equality.
   Only check if values are equal from expected to actual.
   Ignores nil, does not count all keys in actual."
-  (fn [actual expected] [(class actual) (class expected)]))
+  (fn [actual expected]
+    (let [actual-class (class actual)
+          expected-class (class expected)]
+      (cond
+        (or (nil? actual) (nil? expected)) nil
+        (not= actual-class expected-class) :no-match
+        :else actual-class))))
 
-(defmethod to-equal [clojure.lang.PersistentArrayMap clojure.lang.PersistentArrayMap]
+
+(defmethod to-equal nil
   [actual expected]
-  (every? (fn [[k v]] (= v (get actual k))) expected))
+  (do-report {:type :fail :expected expected :actual actual :message "Values are nil"})
+  false)
+
+(defmethod to-equal :no-match
+  [actual expected]
+  (do-report {:type :fail :expected expected :actual actual :message "Classes do not match"})
+  false)
+
+(defmethod to-equal clojure.lang.PersistentArrayMap
+  [actual expected]
+  (let [res (every? (fn [[k v]] (= v (get actual k))) expected)]
+    (if res
+      (do-report {:type :pass :expected expected :actual actual})
+      (do-report {:type :fail :expected expected :actual actual}))
+    res))
 
 (comment
   (to-equal {:foo "bar" :baz "quz"} {:foo "bar"})
