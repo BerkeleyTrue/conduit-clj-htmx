047a27843620cbe11dbffc771f4c1a56043d309b
Berkeley True <berkeleytrue@users.noreply.github.com>
2024-03-12 22:40:53 -0600

    fix(nix): parts not parsn

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parsn.url = "github:hercules-ci/flake-parts";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     boulder.url = "github:berkeleytrue/nix-boulder-banner";
   };
 
