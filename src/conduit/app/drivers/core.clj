fdf3d93ce5cbc2eed397430c5141c72122b53521
Berkeley True <berkeleytrue@users.noreply.github.com>
2024-03-29 08:36:41 -0600

    feat: add articles to routes

--- src/conduit/app/drivers/core.clj
+++ src/conduit/app/drivers/core.clj
@@ -6,7 +6,8 @@
     [conduit.app.drivers.auth :as auth]
     [conduit.app.drivers.layout :refer [render-middleware]]
     [conduit.app.drivers.settings :as settings]
-    [conduit.app.drivers.profile :as profile]))
+    [conduit.app.drivers.profile :as profile]
+    [conduit.app.drivers.articles :as articles]))
 
 (defmethod ig/init-key :app.routes/drivers
   [_ {:keys [on-start-ch user-service]}]
@@ -17,6 +18,7 @@
      [(home/->home-routes)
       (auth/->auth-routes user-service)
       (settings/->settings-routes user-service)
-      (profile/->profile-routes user-service)])])
+      (profile/->profile-routes user-service)
+      (articles/->articles-routes)])])
 
 (derive :app.routes/drivers :app/routes)
